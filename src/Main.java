2e7bc7dd6fac63e41060b0a78bc18ae1c5122bf4
MVBiryukov-teacher-stc <m.biryukov@innopolis.ru>
2018-09-27 12:02:08 +0300

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Какой-то код");
         System.out.println("Суперкод");
-
+        System.out.println("Продолжаем писать");
+        System.out.println("Суперкод");
+        System.out.println("Третий коммит");
+        System.out.println("Еще строка");
     }
 }
